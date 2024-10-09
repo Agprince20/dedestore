7fb54a8a22fb7df7cd03a0b57d4d75a5fcc95f28
Agoro Gafar <agorogafar92@gmail.com>
2024-10-09 12:10:00 +0100

    register

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,11 @@
+import Header from '../component/Header'
+import Register from '../Pages/Register'
+export default function Home() {
+return (
+    <>
+    <Header />
+<h1>Welcome to dedestore</h1>
+
+    </>
+)
+}
