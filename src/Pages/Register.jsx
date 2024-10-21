d34671082b57b42c51bfe7a12447ee7fb9a48068
Agoro Gafar <agorogafar92@gmail.com>
2024-10-21 09:43:21 +0100

    yh

--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -14,7 +14,7 @@ import {
 }
 from 'mdb-react-ui-kit';
 import avatar from "../Assets/avatar.jpg";
-import Header from '../component/Header';
+import Header from '../Components/Header';
 
 export default function Register() {
     // Capturing the User DP Upload
@@ -37,15 +37,63 @@ export default function Register() {
     }
 
     // Capturing Form Data
-    const [firstname, setFirstname] = useState("");
-    const [lastname, setLastname] = useState("");
-    const [email, setEmail] = useState("");
-    const [phone,setphone] = useState("")
-    const [password, setPassword] = useState("");
-    const [confirmPassword, setConfirmPassword] = useState("");
-    const [Username,setusername] = useState("")
-    const [Userrole,setuserrole] = useState("")
+    // const [firstname, setFirstname] = useState("");
+    const [formInput, setFormInput] = useState({
+        firstname: '',
+        lastname: '',
+        email: '',
+        phone: '',
+        password: '',
+        confirmPassword: '',
+        username: "",
+        userRole: "",
+    });
+    // const firstname = formInput.firstname;
+    const {firstname, lastname, email, phone, password, confirmPassword, username, userRole} = formInput;
     // console.log(firstname);
+
+    // Function to handle input
+    const handleInputChange = (e) => {
+        setFormInput({...formInput, [e.target.name]: e.target.value });
+        // setFormInput(prev => ({...prev, [e.target.name]: e.target.value }));
+        // console.log(formInput);
+        // console.log(entry);
+    }
+
+    // Function to handle Registration
+    async function handleFormSubmit () {
+        // Form Validation
+        if(firstname !== "" && lastname !== "" && email !== "" && phone !== "" && password !== "" && confirmPassword !== "" && username !== ""){
+            if (password === confirmPassword) {
+                if(password.length >= 8) {
+                    let formData = new FormData();
+                    formData.append('firstname', firstname);
+                    formData.append('lastname', lastname);
+                    formData.append('email', email);
+                    formData.append('phone', phone);
+                    formData.append('password', password);
+                    formData.append('username', firstname + lastname.charAt(0).toUpperCase() + Math.floor(Math.random() * 1000));
+                    formData.append("user_role", userRole);
+                    formData.append('user_picture', file);
+
+                    let result = await fetch("http://127.0.0.1:8000/api/register", {
+                        method: 'POST',
+                        body: formData
+                    });
+                    result = await result.json();
+                    console.log(result);
+
+                    // console.log(formData.get('email'));
+                } else {
+                    alert("Password should be at least 8 characters long");
+                }
+            } else {
+                alert("Password didn't Match")
+            }
+        } else {
+            alert("Please fill all required fields");
+        }
+    }
 
   return (
     <>
@@ -60,33 +108,33 @@ export default function Register() {
 
                             <div className="d-flex flex-row align-items-center mb-4 gap-2">
                                 <MDBIcon fas icon="user me-3" size='lg'/>
-                                <MDBInput label='Firstname' id='form1' type='text' className='w-100' value={firstname} onChange={(e)=> setFirstname(e.target.value)}/>
-                                <MDBInput label='Lastname' id='form1' type='text' className='w-100' value={lastname} onChange={(e)=> setLastname(e.target.value)} />
+                                <MDBInput label='Firstname' id='form1' type='text' className='w-100' name='firstname' value={firstname} onChange={handleInputChange} />
+                                <MDBInput label='Lastname' id='form1' type='text' className='w-100' name='lastname' value={lastname} onChange={handleInputChange}/>
                             </div>
 
                             <div className="d-flex flex-row align-items-center mb-4 gap-2 wrap">
                                 <MDBIcon fas icon="envelope me-3" size='lg'/>
-                                <MDBInput label=' Email Address' id='form2' type='email' value={email} onChange={(e) => setEmail(e.target.value)}/>
-                                <MDBInput label=' Phone Number' id='form2' type='tel' value={phone} onChange={(e) => setphone(e.target.value)}/>
+                                <MDBInput label=' Email Address' id='form2' type='email' name="email" value={email} onChange={handleInputChange}/>
+                                <MDBInput label=' Phone Number' id='form2' type='tel' name="phone" value={phone} onChange={handleInputChange}/>
                             </div>
 
                             <div className="d-flex flex-row align-items-center mb-4">
                                 <MDBIcon fas icon="lock me-3" size='lg'/>
-                                <MDBInput label='Password' id='form3' type='password' value={password} onChange={(e) => setPassword(e.target.value)}/>
-<MDBInput label='Repeat your password' id='form4' type='password' value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)}/>
+                                <MDBInput label='Password' id='form3' type='password' name='password' value={password} onChange={handleInputChange}/>
+                                <MDBInput label='Repeat your password' id='form4' type='password' name='confirmPassword' value={confirmPassword} onChange={handleInputChange}/>
                             </div>
 
                             <div className="d-flex flex-row align-items-center mb-4 gap-3">
-                                <MDBIcon fas icon="user me-3" size='lg'/>
-                                <MDBInput label='Username' id='form1' type='text' className='w-100' value={Username} onChange={(e) => setusername(e.target.value)}/>
-                                <select class="form-select" aria-label="Default select example" value={Userrole} onChange={(e) => setuserrole(e.target.value)}>
+                                <MDBIcon fas icon="user me-3" size='lg' />
+                                <MDBInput label='Username' id='form1' type='text' className='w-100' name='username' value={username} onChange={handleInputChange}/>
+                                <select class="form-select" aria-label="Default select example" name="userRole" value={userRole} onChange={handleInputChange}>
                                     <option selected>Select Your Role</option>
                                     <option value="user">User</option>
                                     <option value="vendor">Vender</option>
                                 </select>
                             </div>
 
-                        <MDBBtn className='mb-4' size='lg'>Register</MDBBtn>
+                        <MDBBtn className='mb-4' size='lg' onClick={handleFormSubmit}>Register</MDBBtn>
 
                         </MDBCol>
                             <MDBCol md='10' lg='4' className='order-1 order-lg-2 d-flex flex-column'>
